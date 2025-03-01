8b20a6c4e156c03c4784b4205dd821954336054f
Martin Hawks <44511661+Martiman45@users.noreply.github.com>
2025-02-28 22:35:23 -0800

    this is closer to working

--- sdfsc2025/HRMonitor.hpp
+++ sdfsc2025/HRMonitor.hpp
@@ -10,27 +10,33 @@ public:
   int inQueue = 0;
   int inputWire;
   float currentBPM = 0;
-  int* windowBuffer;
+  float* windowBuffer;
   float conVal;
 
   HRMonitor(int bufferSize, int input) {
     avgWindow = bufferSize;
     que = Queue<float>(bufferSize);
     inputWire = input;
-    windowBuffer = new int[bufferSize];
+    windowBuffer = new float[bufferSize];
     for (int i = 0; i < bufferSize; i++){
-      windowBuffer[i] = 0;
+      windowBuffer[i] = 0.0;
     }
-    conVal = 1 / bufferSize;
+    for (int i = 0; i < bufferSize; i++){
+      Serial.println(windowBuffer[i]);
+    }
+    conVal = (1.0 / bufferSize);
   }
 
+  ~HRMonitor() {
+    delete[] windowBuffer; // Ensure to free allocated memory
+  }
 
   void update() {
     if (inQueue == avgWindow) { // If the queue is full, empty it and get a HR
       float last = 0;
       float current = que.pop();
       float next = que.peek();
-      int numPushed = 0;
+      double numPushed = 0;
       for (int i = 1; i < avgWindow; i++) { // pop values in the queue, and save
                                             // if you ever get a local maxima
         last = current;
@@ -38,10 +44,10 @@ public:
         next = que.peek();
         if ((last < current) && (current > next)) {
             tmp.push(i);
-            numPushed++;
+            numPushed = numPushed + 1;
           }
       }
-      float avg = 0;
+      double avg = 0;
       last = 0;
       for (int i = 0; i < numPushed; i++) {
         if (last == 0) {
@@ -53,14 +59,18 @@ public:
       }
       tmp.clear();
       avg = avg / numPushed;
+      Serial.println(avg);
       currentBPM = avg;
       que.clear();
       inQueue = 0;
     }
-    int signal = analogRead(inputWire);
-    // convolve buffer and cycle, appending to queue
-    que.push(this_convolve(signal));
-    inQueue++;
+    float signal = analogRead(inputWire);
+    if ((signal > 1000) || (signal < 0)) {
+      return;
+    } else {
+      que.push(this_convolve(signal));
+      inQueue++;
+    }
   }
 
   int getBPM() {
@@ -68,8 +78,12 @@ public:
   }
 
 private:
-  float this_convolve(int signal) {
+  float this_convolve(float signal) {
     float avg = 0;
+    if ((signal > 1000) || (signal < 0)) {
+      Serial.println("We shouldn't be here");
+      return 500;
+    }
     for (int i = 0; i < avgWindow; i++) {
       avg = avg + (conVal * windowBuffer[i]);
     }
